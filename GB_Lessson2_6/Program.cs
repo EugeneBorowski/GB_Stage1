d88233f410f98cb7dd8998404c74294265fe5012
Eugene <e.borovskiy@yandex.ru>
2021-11-02 20:18:39 +0300

    Все 6 заданий сделаны

---
+++ GB_Lessson2_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GB_Lessson2_6
+{
+    class Program
+    {
+        [Flags]
+        enum dayOfWeek
+        {
+            Понедельник = 0b_0000_0001,
+            Вторник = 0b_0000_0010,
+            Среда = 0b_0000_0100,
+            Четверг = 0b_0000_1000,
+            Пятница = 0b_0001_0000,
+            Суббота = 0b_0010_0000,
+            Воскресенье = 0b_0100_0000
+        }
+        static void Main()
+        {
+            var office1 = (dayOfWeek) 30;
+            var office2 = (dayOfWeek) 127;
+            Console.WriteLine($"Время работы офиса 1: {office1}\n");
+            Console.WriteLine($"Время работы офиса 2: {office2}");
+        }
+    }
+}
