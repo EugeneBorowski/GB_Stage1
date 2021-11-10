5d53d450a357a3ce7f031ce4baae8bd6db88c006
Eugene <e.borovskiy@yandex.ru>
2021-11-10 22:54:56 +0300

    Все 4 задания готовы

---
+++ GB_Lesson4/GB_Lesson4_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GB_Lesson4_2
+{
+    class Program
+    {
+        static int GetNumbers(string str)
+        {
+            var result = 0;
+            var tempstr = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                var currChar = str[i];
+                while (i < str.Length && char.IsDigit(str[i]))
+                {
+                    tempstr += str[i];
+                    i++;
+                }
+
+                if (i == str.Length || !char.IsDigit(str[i]))
+                {
+                    int.TryParse(tempstr,out var tempInt);
+                    result += tempInt;
+                    tempstr = "";
+                }
+            }
+            return result;
+        }
+
+        public static void Main()
+        {
+            Console.Write("Введите числа через пробел: ");
+            var str = Console.ReadLine();
+            Console.WriteLine($"Сумма чисел: {GetNumbers(str)}");
+            Console.ReadLine();
+        }
+    }
+}
