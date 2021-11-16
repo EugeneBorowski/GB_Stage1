3bd56df07bbd7222fb9b6526dba4200d5d8a6cf7
Eugene <e.borovskiy@yandex.ru>
2021-11-16 19:55:08 +0300

    DirTree done

---
+++ GB_Lesson5/GB_Lesson5_DirectoryTree/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace test
+{
+    public class Program
+    {
+        static void RecursiveWriteDir(string file, string path, int dimension)
+        {
+            var separator = new string(' ', dimension);
+            var dirList = new DirectoryInfo(path);
+            DirectoryInfo[] dirs = dirList.GetDirectories();
+            FileInfo[] files = dirList.GetFiles();
+            foreach (var e in dirs)
+            {
+                SaveAndPrint($"{separator}dir: {e.Name}");
+                var tempDir = path + @"\" + e.Name;
+                RecursiveWriteDir(file, tempDir, ++dimension);
+                dimension--;
+            }
+            foreach (var e in files)
+            {
+                SaveAndPrint($"{separator}file: {e.Name}");
+            }
+            void SaveAndPrint(string text)
+            {
+                Console.WriteLine(text);
+                File.AppendAllText(file, $"\n" + text);
+            }
+        }
+
+        static void NoRecursiveWriteDir(string file, string path)
+        {
+            var dirs = new Queue<string>();
+            dirs.Enqueue(path);
+
+            while (dirs.Count > 0)
+            {
+                string dir = dirs.Dequeue();
+                Console.WriteLine("dir: " + dir);
+                File.AppendAllText(file, $"\ndir: {dir}");
+                foreach (var d in Directory.GetDirectories(dir))
+                    dirs.Enqueue(d);
+                foreach (var f in Directory.GetFiles(dir))
+                {
+                    Console.WriteLine("  file: " + f);
+                    File.AppendAllText(file, $"\n  file: {f}");
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            var dimension = 0;
+            var file = "file.txt";
+            Console.Write("Введите путь: ");
+            var path = Console.ReadLine();
+            Console.WriteLine("RecursiveMethod");
+            File.WriteAllText(file, "RecursiveMethod:\n");
+            RecursiveWriteDir(file, path, dimension);
+            Console.WriteLine("NoRecursiveDirectMethod");
+            File.AppendAllText(file, "NoRecursiveDirectMethod:\n");
+            NoRecursiveWriteDir(file, path);
+            Console.ReadLine();
+        }
+    }
+}
